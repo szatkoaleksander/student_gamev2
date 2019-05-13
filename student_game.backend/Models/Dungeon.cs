70a68739cca7bf1aead11c8a682c384271ecf684
aszatko <a.szatko@bonasoft.pl>
2019-05-13 22:52:36 +0200

    Update domain

--- student_game.backend/Models/Dungeon.cs
+++ student_game.backend/Models/Dungeon.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Collections.Generic;
+
 namespace student_game.backend.Models
 {
     public class Dungeon
     {
-        
+        public Guid Id { get; set; }
+        public int Level { get; set; }
+        public virtual ICollection<Enemy> Enemy { get; set; }
+
+        public Dungeon(int level)
+        {
+            Id = Guid.NewGuid();
+            Level = level;
+            Enemy = new List<Enemy>();
+        }
+
+        public Dungeon()
+        {
+            //Id = Guid.NewGuid();
+        }
     }
 }
